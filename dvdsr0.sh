e298762c723d35194107270204576736afda7760
Orangian <tysayahi@gmail.com>
2022-01-15 15:49:26 -0500

    Don't slow down the drive anymore

--- dvdsr0.sh
+++ dvdsr0.sh
@@ -1,7 +1,7 @@
 #!/bin/env bash
 
     #Slow down the drive before anything else
-    eject -x 12 /dev/sr0
+    #eject -x 12 /dev/sr0
 
     #Loop to constantly check if DVD is inserted or not
     while :; do
