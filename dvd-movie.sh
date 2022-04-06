fe5166d3eedbb1995a1fe91044d73fba1b1e1940
Orangian <tysayahi@gmail.com>
2022-04-06 13:08:25 -0400

    Remove dvdsr1.sh, rename dvdsr0.sh and replace sr0 with variable. Further changes to be made later, such as adding functions.

--- dvd-movie.sh
+++ dvd-movie.sh
@@ -1,7 +1,7 @@
 #!/bin/env bash
 
     #Slow down the drive before anything else
-    eject -x 12 /dev/sr1
+    eject -x 12 /dev/${dvddevice}
 
     #Loop to constantly check if DVD is inserted or not
     while :; do
@@ -9,12 +9,12 @@
     clear
 
     #Check if DVD inserted
-    if [[ $(blkid /dev/sr1 > /dev/null; echo $?) == 0 ]]; then
+    if [[ $(blkid /dev/${dvddevice} > /dev/null; echo $?) == 0 ]]; then
         sleep 0.25
         echo "Disk is inserted"
         echo "Checking if DVD is already ripped"
                     mkdir -p ../Backups/DVD/ ;:
-                    currentdvd=$(pre="LABEL=";tmp=$(blkid /dev/sr1 -o export | grep LABEL=) ; echo "${tmp#"$pre"}" | tr -cd '[:alnum:]' | tr '[:upper:]' '[:lower:]')
+                    currentdvd=$(pre="LABEL=";tmp=$(blkid /dev/${dvddevice} -o export | grep LABEL=) ; echo "${tmp#"$pre"}" | tr -cd '[:alnum:]' | tr '[:upper:]' '[:lower:]')
                     dvdrips=$(dir ../Backups/DVD/ | tr -cd '[:alnum:]' | tr '[:upper:]' '[:lower:]')
                     
                         #See if current DVD has already been ripped
@@ -24,22 +24,22 @@
                             echo "Not ripped, starting rip."
 
                             #Rip DVD
-                            dvdbackup -pMi /dev/sr1 -o ../Backups/DVD/
+                            dvdbackup -pMi /dev/${dvddevice} -o ../Backups/DVD/
                         fi
                 
         #Eject DVD after Rip
         echo "Rip completed, ejecting drive"
-        eject /dev/sr1
+        eject /dev/${dvddevice}
         sleep 5
 
     #Check if DVD isn't inserted
-    elif [[ $(blkid /dev/sr1 > /dev/null; echo $?) == 2 ]]; then
+    elif [[ $(blkid /dev/${dvddevice} > /dev/null; echo $?) == 2 ]]; then
         
         #Eject DVD so user can insert
         echo "Disk is not inserted"
         echo "Ejecting drive for insertion"
         echo "Waiting 5 seconds before checking again"
-        eject /dev/sr1
+        eject /dev/${dvddevice}
         sleep 5
         echo
 
@@ -48,6 +48,6 @@
         echo "Could not read if DVD was inserted or not"
         echo "This can sometimes happen if the drive detects the DVD between checks"
         echo "Restarting"
-        eject -n /dev/sr1
+        eject -n /dev/${dvddevice}
     fi
     done
